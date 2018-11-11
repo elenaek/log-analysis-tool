ded678577bf839a81e8ff24a714a1f3781f33af6
Keane Le <keanele12345@gmail.com>
2018-11-11 00:26:49 -0800

    Finished mostpopularauthors function

--- log_tool.py
+++ log_tool.py
@@ -24,4 +24,22 @@ def getMostPopularArticles(num):
     db.close()
     return results
 
+
+def getMostPopularAuthors(num):
+    query = '''    
+        select au.name, count(l.path) as views
+        from articles a, log l, authors au
+        where a.slug = replace (l.path, '/article/','') and a.author = au.id
+        group by au.name
+        order by views desc
+        limit %s
+    '''
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    cursor.execute(query, (str(num), ))
+    results = cursor.fetchall()
+    db.close()
+    return results
+
 print(getMostPopularArticles(3))
+print(getMostPopularAuthors(10))
