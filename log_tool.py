b0137dac5c5769fa13d15e635d1305a281879021
Keane Le <keanele12345@gmail.com>
2018-11-14 00:09:07 -0800

    Reformatted to match style based on pycodestyle. Ignored line warnings for readability of queries and function names

--- log_tool.py
+++ log_tool.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 
-#Import modules
+# Import modules
 import psycopg2
 
-#Name of database
+# Name of database
 DBNAME = "news"
 
 
-#Function that returns the most popular articles, rows limited by numArticles parameter, in descending order (Most popular first)
+# Function that returns the most popular articles,
+# rows limited by numArticles parameter,
+# in descending order (Most popular first)
 def getMostPopularArticles(numArticles):
-    query = '''    
+    query = '''
         select a.title, count(path) as views
         from articles a, log l
         where a.slug = replace(l.path, '/article/','')
@@ -26,9 +28,12 @@ def getMostPopularArticles(numArticles):
     return results
 
 
-#Function that returns the most popular authors (based on view aggregates of their articles), rows limited by numAuthors parameter, in descending order (Most popular first)
+# Function that returns the most popular
+# authors (based on view aggregates of their articles),
+# rows limited by numAuthors parameter,
+# in descending order (Most popular first)
 def getMostPopularAuthors(numAuthors):
-    query = '''    
+    query = '''
         select au.name, count(l.path) as views
         from articles a, log l, authors au
         where a.slug = replace (l.path, '/article/','') and a.author = au.id
@@ -44,9 +49,10 @@ def getMostPopularAuthors(numAuthors):
     return results
 
 
-#Function that retrives the days where the percentage of errors exceed the percent parameter and outputs the date and percentage of errors that day
+# Function that retrives the days where the percentage of errors exceed the
+# percent parameter and outputs the date and percentage of errors that day
 def getErrors(percentErrors):
-    query = '''    
+    query = '''
         select cast(er.errors as float)/cast(tr.total_requests as float)*100 as error_percentage, er.day
         from errors_per_day er, requests_per_day tr
         where er.day = tr.day AND cast(er.errors as float)/cast(tr.total_requests as float)*100 > %s
@@ -59,7 +65,7 @@ def getErrors(percentErrors):
     return results
 
 
-#Function that outputs a report, formatted to match the project criteria
+# Function that outputs a report, formatted to match the project criteria
 def outputFormattedAnswer(numArticles, numAuthors, percentErrors):
     print("Get three most popular articles: ")
     for article in getMostPopularArticles(numArticles):
@@ -76,6 +82,6 @@ def outputFormattedAnswer(numArticles, numAuthors, percentErrors):
         print("{date:%B} {date:%d}, {date:%Y} - {error_percentage}% errors".format(date=day[1], error_percentage=float(round(day[0], 2))))
 
 
-#Calls outputFormattedAnswer and feeds it the parameters to output the properly formatted results based on project criteria:
-#Most popular 3 articles and authors listed in descending order. Days where request errors exceed 1%
-outputFormattedAnswer(3,10,1)
+# Calls outputFormattedAnswer and feeds it the parameters to output the properly formatted results based on project criteria:
+# Most popular 3 articles and authors listed in descending order. Days where request errors exceed 1%
+outputFormattedAnswer(3, 10, 1)
